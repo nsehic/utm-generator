cd30f9565b989f5da94be7f87ba6aa00eff24e26
Nermin Sehic <nsehic@rejectshop.com.au>
2020-10-13 12:06:31 +1100

    Add SEO tags

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import Header from '../components/Header';
 import UTMForm from '../components/UTMForm';
+import Head from 'next/head';
 
 export default function App() {
   return (
     <div className='wrapper'>
+      <Head>
+        <title>UTM Generator | The Reject Shop</title>
+        <link rel="icon" size="16x16" href="https://assets.rejectshop.com.au/careers/assets/icon-16x16.png" />
+        <link rel="icon" size="32x32" href="https://assets.rejectshop.com.au/careers/assets/icon-32x32.png" />
+        <link rel="icon" size="64x64" href="https://assets.rejectshop.com.au/careers/assets/icon-64x64.png" />
+        <link rel="icon" size="128x128" href="https://assets.rejectshop.com.au/careers/assets/icon-128x128.png" />
+      </Head>
       <Header />
       <div className="container">
         <UTMForm/>
