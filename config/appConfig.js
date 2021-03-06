1096695c85dad4f7759b49100ec509f0aa09d589
Nermin Sehic <nsehic@rejectshop.com.au>
2021-03-06 18:17:20 +1100

    Change main page image

--- config/appConfig.js
+++ config/appConfig.js
@@ -3,5 +3,5 @@ export default {
     description: 'Generate a link for social media, email marketing, paid advertising, and more',
     author: 'The Reject Shop',
     version: '1.0.0',
-    image: 'https://images.unsplash.com/photo-1600932717369-d507b606a25d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1259&q=80'
+    image: 'https://images.unsplash.com/photo-1605906696476-0603223941f6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80'
 }
