1a894fa8cf6bd250277a26afa66c9cb596244ee7
Nermin Sehic <nsehic@rejectshop.com.au>
2021-03-06 17:57:34 +1100

    Add Open Graph Tags

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,24 @@ const Index = () => {
         <link rel="icon" size="32x32" href="https://assets.rejectshop.com.au/careers/assets/icon-32x32.png" />
         <link rel="icon" size="64x64" href="https://assets.rejectshop.com.au/careers/assets/icon-64x64.png" />
         <link rel="icon" size="128x128" href="https://assets.rejectshop.com.au/careers/assets/icon-128x128.png" />
+
+        {/* Meta Descriptions */}
+
+        
+        <meta property="og:url" content="https://therejectshop.nsehic.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Link Generator | The Reject Shop" />
+        <meta property="og:description" content="Link generator for The Reject Shop" />
+        <meta property="og:image" content={config.image} />
+
+        
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="therejectshop.nsehic.com" />
+        <meta property="twitter:url" content="https://therejectshop.nsehic.com/" />
+        <meta name="twitter:title" content="Link Generator | The Reject Shop" />
+        <meta name="twitter:description" content="Link generator for The Reject Shop" />
+        <meta name="twitter:image" content={config.image} />
+
       </Head>
       <div className='container'>
         <div className='utm-form'>
