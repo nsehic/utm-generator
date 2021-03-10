ef7b897f24527cb4624ae4a8334242b8fe215495
Nermin Sehic <68089447+nsehic@users.noreply.github.com>
2021-03-10 15:11:18 +1100

    Update Image

--- config/appConfig.js
+++ config/appConfig.js
@@ -3,5 +3,5 @@ export default {
     description: 'Generate a link for social media, email marketing, paid advertising, and more',
     author: 'The Reject Shop',
     version: '1.0.0',
-    image: 'https://images.unsplash.com/photo-1605906696476-0603223941f6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80'
+    image: 'https://images.unsplash.com/photo-1544772546-4357b9384b60?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=564&q=80'
 }
